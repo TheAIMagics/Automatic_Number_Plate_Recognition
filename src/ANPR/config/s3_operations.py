63281594024e5538bb3e9499876115879e6087b5
satyazmx <thakursatya6@gmail.com>
2022-12-19 13:13:12 +0530

    add model pusher component

--- src/ANPR/config/s3_operations.py
+++ src/ANPR/config/s3_operations.py
@@ -66,3 +66,40 @@ class S3Operation:
                 
             except Exception as e:
                 raise CustomException(e, sys) from e
+
+    def upload_file(
+        self,
+        from_filename: str,
+        to_filename: str,
+        bucket_name: str,
+        remove: bool = True,
+    ) -> None:
+
+        """
+        Method Name :   upload_file
+        Description :   This method uploads the from_filename file to bucket_name bucket with to_filename as bucket filename
+        
+        Output      :   Folder is created in s3 bucket
+        """
+        logging.info("Entered the upload_file method of S3Operations class")
+        try:
+            logging.info(
+                f"Uploading {from_filename} file to {to_filename} file in {bucket_name} bucket"
+            )
+
+            self.s3_resource.meta.client.upload_file(
+                from_filename, bucket_name, to_filename
+            )
+            logging.info(
+                f"Uploaded {from_filename} file to {to_filename} file in {bucket_name} bucket"
+            )
+
+            if remove is True:
+                os.remove(from_filename)
+                logging.info(f"Remove is set to {remove}, deleted the file")
+            else:
+                logging.info(f"Remove is set to {remove}, not deleted the file")
+            logging.info("Exited the upload_file method of S3Operations class")
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
